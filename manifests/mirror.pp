79de9babe5d2fa7ff9c79a921c2ac620fece562c
BigGreenOgre <aggibson@cogeco.ca>
2014-11-27 13:07:37 -0500

    Add comments for variable descriptions, add alt_arch var for alternate architectures

--- manifests/mirror.pp
+++ manifests/mirror.pp
@@ -1,4 +1,44 @@
+# = Definition apt_mirror::mirror
 #
+# == Parameters
+#
+# [*mirror*]
+#
+# Hostname or URL for upstream mirror  ( e.g. 'us.archive.ubuntu.com' or 'http://apt.puppetlabs.com' )
+#
+# REQUIRED 
+#
+# [*os*]
+#
+# Name of operating system
+#
+# Default: 'ubuntu'
+#
+# [*release*]
+#
+# Array of release version names to include in mirror ( e.g. [ 'trusty', 'trusty-updates', ] )
+#
+# Default: [ 'precise', ]
+#
+# [*components*]
+#
+# Array of repo components to include in mirror ( e.g. [ 'main', 'restricted', 'universe', 'multiverse', ] )
+#
+# Default: ['main', 'contrib', 'non-free']
+#
+# [*source*]
+#
+# Enable mirroring of source packages. Boolean, true|false
+#
+# Default: false
+#
+# [*alt_arch*]
+#
+# Array of alternate architectures to include in mirror. ( e.g. [ 'i386', 'armel', 'powerpc', ] )
+#
+# Default: undef
+#
+
 define apt_mirror::mirror (
   $mirror,
   $os         = 'ubuntu',
